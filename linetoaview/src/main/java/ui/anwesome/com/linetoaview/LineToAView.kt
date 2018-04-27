8480d030bcf3727776782b97f99152cda6e4e5ea
Anwesh43 <anweshthecool0@gmail.com>
2018-04-27 08:33:17 +0530

    adding LineToAView

--- linetoaview/src/main/java/ui/anwesome/com/linetoaview/LineToAView.kt
+++ linetoaview/src/main/java/ui/anwesome/com/linetoaview/LineToAView.kt
@@ -4,3 +4,24 @@ package ui.anwesome.com.linetoaview
  * Created by anweshmishra on 27/04/18.
  */
 
+import android.content.*
+import android.view.*
+import android.graphics.*
+
+class LineToAView (ctx : Context) : View(ctx) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
